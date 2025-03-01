8ce6e79bd91f3c827cbfc73f8acbe0b3f48ef11c
Fusneica-FlorentinCristian <106351930+Fusneica-FlorentinCristian@users.noreply.github.com>
2025-03-01 14:16:46 +0200

    Time: 64 ms (25.93%) | Memory: 49.8 MB (85.96%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -1,20 +1,20 @@
 class Solution:
-    def minDepth(self, node: Optional[TreeNode]) -> int:
-        if node is None:  
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:  
             return 0
         
         # we'll start at the extremes (leafs)
-        leftDepth = self.minDepth(node.left)
-        rightDepth = self.minDepth(node.right)
+        leftDepth = self.minDepth(root.left)
+        rightDepth = self.minDepth(root.right)
 
         # it is a leaf, so we will start the backtracking by "adding" this level to the total
-        if node.left is None and node.right is None:
+        if root.left is None and root.right is None:
             return 1
         
         # if it has a child, get the length of it
-        if node.left is None:
+        if root.left is None:
             return 1 + rightDepth
-        if node.right is None:
+        if root.right is None:
             return 1 + leftDepth
 
         # if it has two children, get the min of their lengths
